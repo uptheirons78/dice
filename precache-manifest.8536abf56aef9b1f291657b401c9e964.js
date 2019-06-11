31c451701baae8963e495e66676349585a806e5d
Mauro Bono <bono.mauro@gmail.com>
2019-06-11 14:39:08 +0200

    Updates

---
+++ precache-manifest.8536abf56aef9b1f291657b401c9e964.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb9a8bf5b4b3fc0449b10a870154896",
+    "url": "/dice/index.html"
+  },
+  {
+    "revision": "1cfddbb1facbb5b4d1f7",
+    "url": "/dice/static/css/main.20fe473d.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/dice/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "1cfddbb1facbb5b4d1f7",
+    "url": "/dice/static/js/main.6cecefa4.chunk.js"
+  },
+  {
+    "revision": "31385d9ad14271274f77",
+    "url": "/dice/static/js/runtime~main.fe841fa0.js"
+  }
+]);
